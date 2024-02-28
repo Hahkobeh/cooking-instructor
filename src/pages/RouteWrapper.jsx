40a7d3ecd2d88d8a1bfd22e7b7d923916059eb67
Jacob <hahkobeh@gmail.com>
2024-02-27 20:08:12 -0700

    added phone bar

--- src/pages/RouteWrapper.jsx
+++ src/pages/RouteWrapper.jsx
@@ -4,6 +4,19 @@ import { Outlet } from 'react-router-dom';
 const RouteWrapper = () => {
 	return (
 		<>
+			<header>
+				<div>
+					<p>57%</p>
+					<span className="material-symbols-outlined">mark_email_unread</span>
+					<span className="material-symbols-outlined">sms</span>
+				</div>
+				<div>
+					<span className="material-symbols-outlined">do_not_disturb_on</span>
+					<span className="material-symbols-outlined">wifi</span>
+					<span className="material-symbols-outlined">battery_4_bar</span>
+					<p>57%</p>
+				</div>
+			</header>
 			<main>
 				<Outlet />
 			</main>

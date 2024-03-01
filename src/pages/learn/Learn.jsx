7bd31782a40eeef311505c15163e55270fce582d
mohammad-fahd <mohammad.fahd@ucalgary.ca>
2024-03-01 02:17:47 -0700

    Learn more screen from Adobe XD prototype implemented

--- src/pages/learn/Learn.jsx
+++ src/pages/learn/Learn.jsx
@@ -1,17 +1,62 @@
-// import styles from './learn.module.scss';
-import NavBar from "@/components/nav-bar/NavBar";
+import styles from './learn.module.scss';
+//import Card from "@/components/card/Card";
+//import NavBar from "@/components/nav-bar/NavBar";
+
+//TODO: Make cards clickable to go to either a technique or ingredients screen
+//TODO: Make design for technique/ingredients screen
 
 const Learn = () => {
 	return (
 		<div>
-			<h1>Hello</h1>
-			<body>
+			<h1>Learn more</h1>
+			<div style={{marginTop: '16px'}}>
+				<h2>Techniques</h2>
+			</div>
+			<ul>
+				<li>
+					<div className={styles.card}>
+						<h3>Sous Vide</h3>
+						<h4>Cook food to perfection - using water</h4>
+					</div>
+					<div className={styles.card}>
+						<h3>Cutting</h3>
+						<h4>Chop chop, but not your fingers...</h4>
+					</div>
+					<div className={styles.card}>
+						<h3>Sous Vide</h3>
+						<h4>Cook food to perfection - using water</h4>
+					</div>
+					<div className={styles.card}>
+						<h3>Cutting</h3>
+						<h4>Chop chop, but not your fingers...</h4>
+					</div>
+				</li>
+			</ul>
+			<div style={{marginTop: '30px', borderWidth: '50px', borderColor: 'red'}}>
+				<div style={{marginTop: '16px'}}>
+					<h2>Ingredients</h2>
+				</div>
 				<ul>
 					<li>
-						<NavBar/>
+						<div className={styles.card}>
+							<h3>Onions</h3>
+							<h4>Chefs May Cry</h4>
+						</div>
+						<div className={styles.card}>
+							<h3>Tomatoes</h3>
+							<h4>Master the fruitgetable</h4>
+						</div>
+						<div className={styles.card}>
+							<h3>Onions</h3>
+							<h4>Chefs May Cry</h4>
+						</div>
+						<div className={styles.card}>
+							<h3>Tomatoes</h3>
+							<h4>Master the fruitgetable</h4>
+						</div>
 					</li>
 				</ul>
-			</body> 
+			</div>
 		</div>
 	);
 };

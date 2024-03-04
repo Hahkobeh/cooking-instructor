8cf095fcb8f0a82239ca3d75710c876b6976a870
Achraf <achraf.abdelrhafour@outlook.com>
2024-03-03 17:07:48 -0700

    Turned recipe ingredient cards into collapsable dropdowns.

--- src/components/shopping/ShoppingListRecipe.jsx
+++ src/components/shopping/ShoppingListRecipe.jsx
@@ -1,33 +1,53 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import ShoppingListIngredient from './ShoppingListIngredient';
 import styles from './shopping.module.scss';
 
 // Define the ShoppingListRecipe functional component with props received from the parent component (ShoppingList)
 const ShoppingListRecipe = ({ recipes, onIngredientToggle }) => {
+	// State to manage dropdown visibility
+	const [dropdownsOpen, setDropdownsOpen] = useState({});
+
+	// Function to toggle dropdown visibility
+	const toggleDropdown = (id) => {
+		setDropdownsOpen((prevDropdownsOpen) => ({
+			...prevDropdownsOpen,
+			[id]: !prevDropdownsOpen[id],
+		}));
+	};
+
 	return (
 		<div>
 			{/* Iterate over the recipes array passed as a prop, rendering a div for each recipe */}
 			{recipes.map((recipe) => (
 				<div key={recipe.id} className={styles['shopping-list-recipe']}>
-					<h3 className={styles['recipe-title']}>{recipe.name}</h3>
-					<ul>
-						{/* Iterate over the ingredients of the current recipe, rendering a list item for each */}
-						{recipe.ingredients.map((ingredient, index) => (
-							<li key={index}>
-								{/* Render the ShoppingListIngredient component for each ingredient */}
-								<ShoppingListIngredient
-									ingredient={ingredient} // Pass the entire ingredient object
-									onToggle={() => onIngredientToggle(recipe.id, ingredient)} // Calls onIngredientToggle, passed as a prop (from the parent component)
-								/>
-							</li>
-						))}
-					</ul>
+					<h3
+						className={`${styles['recipe-title']} ${dropdownsOpen[recipe.id] ? styles['rotate'] : ''}`}
+						onClick={() => toggleDropdown(recipe.id)}
+					>
+						{recipe.name}
+					</h3>
+					{dropdownsOpen[recipe.id] && (
+						<ul>
+							{/* Iterate over the ingredients of the current recipe, rendering a list item for each */}
+							{recipe.ingredients.map((ingredient, index) => (
+								<li key={index}>
+									{/* Render the ShoppingListIngredient component for each ingredient */}
+									<ShoppingListIngredient
+										ingredient={ingredient}
+										onToggle={() => onIngredientToggle(recipe.id, ingredient)} // Calls onIngredientToggle, passed as a prop (from the parent component)
+									/>
+								</li>
+							))}
+						</ul>
+					)}
 				</div>
 			))}
 		</div>
 	);
 };
 
+// Proptypes definition
 ShoppingListRecipe.propTypes = {
 	recipes: PropTypes.arrayOf(
 		PropTypes.shape({
@@ -38,7 +58,7 @@ ShoppingListRecipe.propTypes = {
 					name: PropTypes.string.isRequired,
 					quantity: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 						.isRequired,
-					checked: PropTypes.bool, // Ensure this is included
+					checked: PropTypes.bool,
 				})
 			).isRequired,
 		})

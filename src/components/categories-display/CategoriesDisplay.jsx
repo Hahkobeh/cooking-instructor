1a0f3314475fc175a49abdff032df675cda5e636
Achraf <achraf.abdelrhafour@outlook.com>
2024-04-04 21:36:41 -0600

    Merged categories and tags into one master "tags" list

--- src/components/categories-display/CategoriesDisplay.jsx
+++ src/components/categories-display/CategoriesDisplay.jsx
@@ -1,8 +1,8 @@
-import { useCategories } from '../../context/data/useCategories';
+import { useTags } from '@/context/data/useTags';
 import styles from './categories-display.module.scss';
 
 const CategoriesDisplay = () => {
-	const categories = useCategories();
+	const categories = useTags();
 
 	return (
 		<div className={styles['categories-container']}>
@@ -10,7 +10,7 @@ const CategoriesDisplay = () => {
 			{categories.map((category) => (
 				<div key={category.id} className={styles['category-card']}>
 					<span className="material-symbols-outlined">{category.icon}</span>
-					<span className={styles['category-name']}>{category.name}</span>
+					<span className={styles['category-name']}>{category.value}</span>
 				</div>
 			))}
 		</div>

f8b9eac106a90f6a04e9c070776730c0cae557c6
mohammad-fahd <mohammad.fahd@ucalgary.ca>
2024-04-08 07:20:29 -0600

    Added thank you message after rating

--- src/pages/recipe/sub-views/Steps.jsx
+++ src/pages/recipe/sub-views/Steps.jsx
@@ -1,17 +1,21 @@
 import RatingForm from '@/components/rating-form/RatingForm';
 import { useRecipes } from '@/context/data/useRecipes';
 import { useNavigate, useParams } from 'react-router-dom';
+import Dialog from '@/components/dialog/Dialog';
 import styles from './steps.module.scss';
+import { useState } from 'react';
 
 const Steps = () => {
 	const navigate = useNavigate(); // get the navigate function from the router
 	const { recipeId } = useParams(); // get the recipeId from the URL
 	const recipes = useRecipes(); // get recipes from the context
 	const recipe = recipes.find((r) => r.id.toString() === recipeId);
+	const [thankOn, setThankOn] = useState(false);
 
 	const handleRatingSubmit = (newRating) => {
 		// eventually will use to update the recipe rating in state/context
 		console.log(`New rating submitted: ${newRating}`);
+		setThankOn(true);
 	};
 
 	if (!recipe) {
@@ -24,6 +28,12 @@ const Steps = () => {
 	};
 
 	return (
+		<>
+		{thankOn && (
+			<Dialog close={() => setThankOn(false)} title="Thanks!">
+				<p style={{ width: '300px' }}>Thank you for providing your rating!</p>
+			</Dialog>
+		)}
 		<div className={styles['steps-container']}>
 			<button
 				className={styles['step-mode-button']}
@@ -44,6 +54,7 @@ const Steps = () => {
 			</h3>
 			<RatingForm onSubmit={handleRatingSubmit} />
 		</div>
+		</>
 	);
 };
 

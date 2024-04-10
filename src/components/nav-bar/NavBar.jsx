8776423a6fee3ea0a88f1ddf5ce1fe30f5ded7e7
Achraf <achraf.abdelrhafour@outlook.com>
2024-04-09 22:59:45 -0600

    NavBar now highlights home icon when on recipe view

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,8 +1,17 @@
 import { routerData } from '@/pages/routeData';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import styles from './nav-bar.module.scss';
 
 const NavBar = () => {
+	const location = useLocation();
+	const isActiveRoute = (routePath) => {
+		// check if the current location should activate the "Home" icon
+		if (routePath === '/' && location.pathname.startsWith('/recipe')) {
+			return true;
+		}
+		// fall back to the default behavior for other links
+		return location.pathname === routePath;
+	};
 	return (
 		<ul className={styles.navBar}>
 			{routerData
@@ -11,7 +20,7 @@ const NavBar = () => {
 					<li key={route.path}>
 						<NavLink
 							to={route.path}
-							className={({ isActive }) => (isActive ? styles.active : '')}
+							className={() => (isActiveRoute(route.path) ? styles.active : '')}
 						>
 							<span className="material-symbols-outlined">{route.icon}</span>
 						</NavLink>
